2e49f3418a502a56f1c8e1eeb8229c62a55b9513
ThomasPTurner <ThomasPTurner@googlemail.com>
2019-07-22 16:05:19 +0100

    add styling to error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
+import '../styles/Error.css'
 
 class Error extends Component {
     render() {
-        if (!this.props.location.state) {
-            return (
-            <div>
-                <h1>error 404</h1>
-                <h3>page not found</h3>
-            </div>
-            )
-        }
-        const { code, msg } = this.props.location.state
+        const { state } = this.props.location
+        const [code, msg] = state ? [state.code, state.msg] : [404, 'page not found']
         return (
-            <div>
+            <div className="Error">
                 <h1>error {code}</h1>
                 <h3>{msg}</h3>
             </div>
